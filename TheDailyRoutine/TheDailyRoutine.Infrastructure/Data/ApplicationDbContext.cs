dfb9772d788f6c2cae497a13a3aa7a00c379c51d
VladiGanchev <160647367+VladiGanchev@users.noreply.github.com>
2024-12-01 21:20:37 +0200

    Created different layers for logic

--- TheDailyRoutine/TheDailyRoutine.Infrastructure/Data/ApplicationDbContext.cs
+++ TheDailyRoutine/TheDailyRoutine.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace TheDailyRoutine.Data
+namespace TheDailyRoutine.Infrastructure.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
--- TheDailyRoutine/TheDailyRoutine/Program.cs
+++ TheDailyRoutine/TheDailyRoutine/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using TheDailyRoutine.Data;
+using TheDailyRoutine.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
