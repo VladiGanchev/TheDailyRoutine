a0a7f302c630d87f62e0a2864ee1b313e4eaae9e
VladiGanchev <160647367+VladiGanchev@users.noreply.github.com>
2024-12-01 21:00:01 +0200

    Added connection string

--- TheDailyRoutine/TheDailyRoutine/Program.cs
+++ TheDailyRoutine/TheDailyRoutine/Program.cs
@@ -4,15 +4,12 @@ using TheDailyRoutine.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddApplicationDbContext(builder.Configuration);
+builder.Services.AddApplicationIdentity(builder.Configuration);
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddApplicationServices();
 
 var app = builder.Build();
 
