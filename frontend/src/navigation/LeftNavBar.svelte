fc95e1fb956fffda0520691be1a2322dd3df775b
Ognian Baruh <ognian@baruh.net>
2024-12-16 21:15:55 +0200

    Updated left navigation bar to include the routes to the components

--- frontend/src/navigation/LeftNavBar.svelte
+++ frontend/src/navigation/LeftNavBar.svelte
@@ -1,7 +1,40 @@
 <script>
-	let my_habits_selector = ["My Habits", "Calendar", "Statistics"];
-	let add_habits_selector = ["Create a Habit", "Pick a Habit"]
-	let settings = ["Account", "Logout"]
+	let my_habits_selector = [
+		{
+			name: "My Habits",
+			path: "/#/my-habits"
+		},
+		{
+			name: "Calendar",
+			path: "/#/calendar"
+		},
+		{
+			name: "Statistics",
+			path: "/#/statistics"
+		}
+	];
+
+	let add_habits_selector = [
+		{
+			name: "Create a Habit",
+			path: "/#/create-habit"
+		},
+		{
+			name: "Pick a Habit",
+			path: "/#/pick-habit"
+		}
+	];
+
+	let settings = [
+		{
+			name: "Account",
+			path: "/#/account"
+		},
+		{
+			name: "Logout",
+			path: "/#/logout"
+		}
+	];
 </script>
 
 <div class="nav-bar">
@@ -12,21 +45,27 @@
 	<div class="section">
 		<div class="section-title">MY HABITS</div>
 		{#each my_habits_selector as my_habit}
-			<div class="list-item">{my_habit}</div>
+			<div class="list-item">
+				<a class="item-name" href={my_habit.path}>{my_habit.name}</a>
+			</div>
 		{/each}
 	</div>
 
 	<div class="section">
 		<div class="section-title">ADD HABITS</div>
 		{#each add_habits_selector as add_habit}
-			<div class="list-item">{add_habit}</div>
+			<div class="list-item">
+				<a class="item-name" href={add_habit.path}>{add_habit.name}</a>
+			</div>
 		{/each}
 	</div>
 
 	<div class="section">
 		<div class="section-title">SETTINGS</div>
 		{#each settings as setting}
-			<div class="list-item">{setting}</div>
+			<div class="list-item">
+				<a class="item-name" href={setting.path}>{setting.name}</a>
+			</div>
 		{/each}
 	</div>
 
@@ -51,6 +90,8 @@
         justify-content: center;
         padding: 0.5rem 0;
         border-bottom: 1px solid #333;
+				font-weight: bolder;
+				font-size: 1.2rem;
     }
 
     .section {
@@ -69,11 +110,11 @@
         border-radius: 4px;
     }
 
-    .list-item:hover {
-        background-color: #333;
+    .list-item:hover .item-name {
+				color: #888;
     }
 
-    .icon {
-        margin-right: 0.5rem;
-    }
+		.item-name {
+				color: #fff;
+		}
 </style>
