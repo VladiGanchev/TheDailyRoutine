2321c06bf4747535296213d3ddd3ee5f760e26f9
PetkoDeyanov <petkopetkov1001@gmail.com>
2024-12-23 15:43:01 +0200

    Models for ApiControllers (Requests and Responses)

---
+++ TheDailyRoutine/TheDailyRoutine.Core/Models/API/Responses/Common/PaginatedResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheDailyRoutine.Core.Models.API.Responses.Common
+{
+    public class PaginatedResponse<T>
+    {
+        // todo: zanimava li mi se
+        public IEnumerable<T> Items { get; set; } = new List<T>();
+        public int TotalItems { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public bool HasNextPage { get; set; }
+        public bool HasPreviousPage { get; set; }
+    }
+}
---
+++ TheDailyRoutine/TheDailyRoutine.Core/Models/API/Responses/Statistics/StatusSummaryResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheDailyRoutine.Core.Models.API.Responses.Statistics
+{
+    public class StatsSummaryResponse
+    {
+        public double OverallCompletionRate { get; set; }
+        public int TotalHabits { get; set; }
+        public int ActiveHabits { get; set; }
+        public int LongestStreak { get; set; }
+        public List<string> TopPerformingHabits { get; set; } = new List<string>();
+        public List<string> AreasForImprovement { get; set; } = new List<string>();
+    }
+}
