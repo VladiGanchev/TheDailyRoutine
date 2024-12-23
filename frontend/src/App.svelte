0e847238b805191c6f3e5af4ab4ed5179eaddeea
PetkoDeyanov <petkopetkov1001@gmail.com>
2024-12-23 16:21:40 +0200

    Testing ViewMapping

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -19,80 +19,88 @@
 	import Today from './habits/Today.svelte';
 
 	if (!window.location.hash) {
-		navigateTo(window.location.pathname);
-		window.location.pathname = "";
+	navigateTo(window.location.pathname);
+	window.location.pathname = "";
 	}
 
 	let currentUser;
 	$: currentUser = $user
 
 	const validateLogin = () => {
-		return currentUser !== null;
+	return currentUser !== null;
 	}
 
 	const validateLoginFailed = () => {
-		navigateTo("/");
+	navigateTo("/");
 	}
 
 	// Define the route configuration
 	const routes = {
-		"/": Home,
-		"/account": wrap({
-			component: LeftNavBar,
-			props: { component: MyAccount },
-			conditions: [() => {
-				return validateLogin();
-			}]
-		}),
-		"/today": wrap({
-			component: LeftNavBar,
-			props: { component: Today },
-			conditions: [() => {
-				return validateLogin();
-			}]
-		}),
-		"/calendar": wrap({
-			component: LeftNavBar,
-			props: { component: Calendar },
-			conditions: [() => {
-				return validateLogin();
-			}]
-		}),
-		"/statistics": wrap({
-			component: LeftNavBar,
-			props: { component: Statistics },
-			conditions: [() => {
-				return validateLogin();
-			}]
-		}),
-		"/my-habits": wrap({
-			component: LeftNavBar,
-			props: { component: MyHabits },
-			conditions: [() => {
-				return validateLogin();
-			}]
-		}),
-		"/create-habit": wrap({
-			component: LeftNavBar,
-			props: { component: CreateHabit },
-			conditions: [() => {
-				return validateLogin();
-			}]
-		}),
-		"/pick-habits": wrap({
-			component: LeftNavBar,
-			props: { component: PickHabits },
-			conditions: [() => {
-				return validateLogin();
-			}]
-		}),
-		"/logout": wrap({
-			component: Logout,
-			conditions: [() => {
-				return validateLogin();
-			}]
-		}),
-		"*": NotFound
+	"/": Home,
+	"/account": wrap({
+	component: LeftNavBar,
+	props: { component: MyAccount },
+	conditions: [() => {
+	return validateLogin();
+	}]
+	}),
+	"/today": wrap({
+	component: LeftNavBar,
+	props: { component: Today },
+	conditions: [() => {
+	return validateLogin();
+	}]
+	}),
+	"/calendar": wrap({
+	component: LeftNavBar,
+	props: { component: Calendar },
+	conditions: [() => {
+	return validateLogin();
+	}]
+	}),
+	"/statistics": wrap({
+	component: LeftNavBar,
+	props: { component: Statistics },
+	conditions: [() => {
+	return validateLogin();
+	}]
+	}),
+	"/my-habits": wrap({
+	component: LeftNavBar,
+	props: { component: MyHabits },
+	conditions: [() => {
+	return validateLogin();
+	}]
+	}),
+	"/edit-habit/:id": wrap({
+	component: LeftNavBar,
+	props: { component: CreateHabit },
+	conditions: [() => {
+	return validateLogin();
+	}]
+	}),
+
+	"/create-habit": wrap({
+	component: LeftNavBar,
+	props: { component: CreateHabit },
+	conditions: [() => {
+	return validateLogin();
+	}]
+	}),
+	"/pick-habits": wrap({
+	component: LeftNavBar,
+	props: { component: PickHabits },
+	conditions: [() => {
+	return validateLogin();
+	}]
+	}),
+	"/logout": wrap({
+	component: Logout,
+	conditions: [() => {
+	return validateLogin();
+	}]
+	}),
+	"*": NotFound
 	};
 </script>
 
